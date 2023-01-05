ddac579495b32e269074aa067f677faebdf886ac
Piotrek Neupaueer <piotrekneupaueer@piotreks-mac-mini.home>
2023-01-05 14:15:46 +0100

    add category page and start with styled components

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
-
+import { useNavigate } from 'react-router-dom';
+import { useContext } from 'react';
+import { CartContext } from '../../context/cart.context';
 import Button from '../button/button.component';
+import CartItem from '../cart-item/cart-item.component';
 
 import './cart-dropdown.styles.scss';
 
-const CartDropdown = () => (
+const CartDropdown = () => {
+  const {cartItems} = useContext(CartContext)
+  const navigate = useNavigate()
+
+  const goToCheckoutHandler = () =>{
+    navigate('/checkout')
+  }
+
+
+  return(
   <div className='cart-dropdown-container'>
     <div className='cart-items' />
-    <Button> GO TO CHECKOUT</Button>
+    {cartItems.map(item => <CartItem key={item.id} cartItem={item} />)}
+    <Button onClick={goToCheckoutHandler}>CHECKOUT</Button>
   </div>
-);
+  );
+}
 
 export default CartDropdown;
