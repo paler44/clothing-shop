b58a4e0a84f5a8c3ee7c124483d51031a3b15d28
Piotrek Neupaueer <piotrekneupaueer@piotreks-mac-mini.home>
2023-01-05 14:16:29 +0100

    add-styled-componentes-and-category-page

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,5 +1,5 @@
-import { useParams } from 'react-router-dom'
-import { useContext, useState, useEffect } from 'react'
+import { useParams  } from 'react-router-dom'
+import { useContext, useState, useEffect, Fragment } from 'react'
 import './category.styles.scss'
 import { CategoriesContext } from '../../context/categories.context'
 import ProductCard from '../../components/product-card/product-card.component'
@@ -15,10 +15,14 @@ const Category = () =>{
    }, [category, categoriesMap])
 
    return (
+    <Fragment>
+    <h2 className='title'>{category.toUpperCase()}</h2>
     <div className='category-container'>
+        
         {products &&
         products.map((product)=><ProductCard key={product.id} product={product}/>)}
     </div>
+    </Fragment>
    )
 }
 
