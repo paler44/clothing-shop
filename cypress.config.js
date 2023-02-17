47fab60fe5e23bb76c61ff0964f5e8ee036a564f
paler44 <piotrneupauer@gmail.com>
2023-02-17 05:17:48 +0100

    add-slack-integration

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,10 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
-    baseUrl: 'http://localhost:3000',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
   
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,10 +1,10 @@
 describe('empty spec', () => {
-  it('passes', () => {
+  it('go to shop page', () => {
     cy.visit('http://localhost:3000/shop')
     cy.get('[cy-data="item-id-2"]').click({force:true})
-    cy.get('[cy-data="cart-icon"]').click({force:true})
-    cy.get('[cy-data="checkout-button"]').click({force:true})
-    cy.get('[cy-data="cart-icon"]').click({force:true})
-    cy.get('[cy-data="remove-button"]').click({force:true})
+    cy.get('[cy-data="cart-icon"]').click()
+    cy.get('[cy-data="checkout-button"]').should('contain', 'CHECKOUT').click()
+    cy.get('[cy-data="cart-icon"]').click()
+    cy.get('[cy-data="remove-button"]').click()
   })
 })
