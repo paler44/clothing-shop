c7334d6dac5c36594583aa6a94d8a3d1fccf4de7
paler44 <piotrneupauer@gmail.com>
2023-02-17 05:38:27 +0100

    test7

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,11 +1,12 @@
 describe('empty spec', () => {
   it('go to shop page', () => {
     cy.visit('http://localhost:3000/shop')
-    cy.log('xd')
-    cy.get('[cy-data="item-id-2"]').click({force:true})
-    cy.get('[cy-data="cart-icon"]').click()
-    cy.get('[cy-data="checkout-button"]').should('contain', 'CHECKOUT').click()
-    cy.get('[cy-data="cart-icon"]').click()
-    cy.get('[cy-data="remove-button"]').click()
+    
+    // cy.log('xd')
+    // cy.get('[cy-data="item-id-2"]').click({force:true})
+    // cy.get('[cy-data="cart-icon"]').click()
+    // cy.get('[cy-data="checkout-button"]').should('contain', 'CHECKOUT').click()
+    // cy.get('[cy-data="cart-icon"]').click()
+    // cy.get('[cy-data="remove-button"]').click()
   })
 })
