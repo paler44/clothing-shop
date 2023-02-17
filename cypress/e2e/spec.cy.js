9c622b08040cf62882387d346e31141103305762
paler44 <piotrneupauer@gmail.com>
2023-02-17 05:58:05 +0100

    test14

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,15 +2,9 @@ describe('empty spec', () => {
   it('go to shop page', () => {
     cy.visit('/shop')
     cy.get('[cy-data="item-id-2"]').click({force:true})
-    
-    // cy.log('xd')
-    // cy.get('[cy-data="item-id-2"]').click({force:true})
-    // cy.get('[cy-data="cart-icon"]').click()
-    // cy.get('[cy-data="checkout-button"]').should('contain', 'CHECKOUT').click()
-    // cy.get('[cy-data="cart-icon"]').click()
-    // cy.get('[cy-data="remove-button"]').click()
-  })
-  it('huehue', () =>{
-    
+    cy.get('[cy-data="cart-icon"]').click()
+    cy.get('[cy-data="checkout-button"]').should('contain', 'CHECKOUT').click()
+    cy.get('[cy-data="cart-icon"]').click()
+    cy.get('[cy-data="remove-button"]').click()
   })
 })
