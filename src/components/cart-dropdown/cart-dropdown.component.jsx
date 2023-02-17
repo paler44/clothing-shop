32f979370ae12a4a3025768fb71a94b4190bae89
Piotrek Neupaueer <piotrekneupaueer@Piotreks-Mac-mini.local>
2023-02-17 04:04:37 +0100

    test

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -20,7 +20,7 @@ const CartDropdown = () => {
   <div className='cart-dropdown-container'>
     <div className='cart-items' />
     {cartItems.map(item => <CartItem key={item.id} cartItem={item} />)}
-    <Button onClick={goToCheckoutHandler} cy-data="cart-icon">CHECKOUT</Button>
+    <Button onClick={goToCheckoutHandler} cy-data="cartt-icon">CHECKOUT</Button>
   </div>
   );
 }
