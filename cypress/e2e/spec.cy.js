7c39cb6b68d2235a3f244ce90b776631b447ae84
paler44 <piotrneupauer@gmail.com>
2023-02-17 06:30:29 +0100

    huehue2

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,7 +1,7 @@
 describe('empty spec', () => {
   it('go to shop page', () => {
     cy.visit('/shop')
-    cy.get('[cy3-data="item-id-2"]').click({force:true})
+    cy.get('[cy-data="item-id-2"]').click({force:true})
     cy.get('[cy-data="cart-icon"]').click()
     cy.get('[cy-data="checkout-button"]').should('contain', 'CHECKOUT').click()
     cy.get('[cy-data="cart-icon"]').click()
