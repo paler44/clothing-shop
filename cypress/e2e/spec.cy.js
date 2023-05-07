1b22b15bdb1b8212109bf5a9cf50ec3e722a70d3
paler44 <piotr.neupauer@dietly.pl>
2023-05-07 14:46:09 +0200

    test22

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,12 +1,9 @@
 describe('empty spec', () => {
   it('go to shop page', () => {
-    cy.visit('/shop')
+    cy.visit('https://example.com/')
     cy.log('xD')
-    cy.get('[cy-data="item-id-2"]').click({force:true})
-    cy.get('[cy-data="cart-icon"]').click()
-    cy.get('[cy-data="checkout-button"]').should('contain', 'CHECKOUT').click()
-    cy.get('[cy-data="cart-icon"]').click()
-    cy.get('[cy-data="remove-button"]').click()
+    cy.get('h1').should('be.visible')
+    
     //test
   })
 })
